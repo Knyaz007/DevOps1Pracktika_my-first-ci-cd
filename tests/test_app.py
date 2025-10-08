f06ebe3bcf1600e8949ad4bd00faab73ea6cc256
Владимир Хабалов <y-men.y-men.y-men@mail.ru>
2025-10-08 15:00:06 +0300

    Add CI/CD pipeline with SAST scanning - new Flask==3.1.2 v2

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,16 @@
 # tests/test_app.py
-import sys
-import os
+import pytest
 
-# Добавляем корень проекта в sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import DevOps1Pracktika_my_first_ci_cd  # теперь Python видит app.py в корне
+# Вариант 1: если у тебя файл app.py в корне проекта
+try:
+    from app import app
+except Exception:
+    # Вариант 2: если твой пакет называется DevOps1Pracktika_my_first_ci_cd
+    from DevOps1Pracktika_my_first_ci_cd import app  # fallback
 
 def test_hello_world():
-    with DevOps1Pracktika_my_first_ci_cd.test_client() as client:
-     response = client.get('/')
-    assert response.status_code == 200
-    assert b'Hello' in response.data
+    # Используем тест-клиент самого Flask-приложения
+    with app.test_client() as client:
+        response = client.get('/')  # GET /
+        assert response.status_code == 200
+        assert b"Hello, user" in response.data
