da73a8a8b0c1394fccce9e216070455618a308e3
Владимир Хабалов <y-men.y-men.y-men@mail.ru>
2025-10-08 14:52:59 +0300

    Add CI/CD pipeline with SAST scanning - new

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,14 @@
 # tests/test_app.py
-import pytest
-from app import app
+import sys
+import os
+
+# Добавляем корень проекта в sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import DevOps1Pracktika_my_first_ci_cd  # теперь Python видит app.py в корне
+
 def test_hello_world():
-    with app.test_client() as client:
+    with DevOps1Pracktika_my_first_ci_cd.test_client() as client:
      response = client.get('/')
     assert response.status_code == 200
     assert b'Hello' in response.data
